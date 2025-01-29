28a284efd72d57784635645559140932dae9d6d2
Johanna Huaraca <86489770+Jahe01@users.noreply.github.com>
2025-01-29 14:22:20 -0500

    Delete src directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-const router =Router()
-router.get('/', (req, res) => res.render('index', { title: 'Aprende Jugando'}))
-router.get('/educativo', (req, res) => res.render('educativo', { title: 'Aprende Jugando'}))
-router.get('/instrucciones', (req, res) => res.render('instrucciones', { title: 'Aprende Jugando'}))
-router.get('/juego', (req, res) => res.render('juego', { title: 'Aprende Jugando'}))
-
-export default router
